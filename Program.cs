324d6e42078d4ed529ba31322d5941dc087a5e1b
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-27 22:59:16 +0530

    checking for merge with uc3 and master

--- Program.cs
+++ Program.cs
@@ -9,24 +9,48 @@ namespace EmployeProblem
             int Is_Full_Time = 1;
             Random randomnum = new Random();
             int EmpCheck = randomnum.Next(0, 3);
-            int full_time_salary = 20*8;
+            int full_time_salary = 20 * 8;
             int parttimesalary = 4 * 20;
 
-            if (EmpCheck == Is_Full_Time)
-            {
-                Console.WriteLine("Employee is Present");
-                Console.WriteLine("Daily salary of Employe {0}",full_time_salary);
 
-            }
-            else if (EmpCheck==2)
+            switch (EmpCheck)
             {
-                Console.WriteLine("employee is Part Time");
-                Console.WriteLine("Daily salary of Employe {0}", parttimesalary);
+                case 1:
+
+                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Daily salary of Employe {0}", full_time_salary);
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("employee is Part Time");
+                    Console.WriteLine("Daily salary of Employe {0}", parttimesalary);
+                    break;
+
+                case 0:
+                    Console.WriteLine("Employe is absent");
+                    break;
             }
-            else
+
+            int total_Emp_hrs = 0;
+            for (int i = 1; i <= 20; i++)
             {
-                Console.WriteLine("Employe is absent");
+                switch (randomnum.Next(0, 3))
+                {
+                    case 1:
+                        total_Emp_hrs += 8;//emp present for full day on that working day
+                        break;
+                    case 2:
+                        total_Emp_hrs += 4; //emp present for part time on that working day
+                        break;
+                    case 0:
+                        break;
+                }
             }
+            Console.WriteLine("No of working hours of employee in a month are {0}", total_Emp_hrs);
+            int monthly_Wage = total_Emp_hrs * 20;
+            Console.WriteLine("Montly wage of employee is {0}", monthly_Wage);
         }
     }
+
 }

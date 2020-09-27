77fd108790cb11c96826601a265edfad09aab815
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-27 22:38:09 +0530

    checking for merge with uc1 and master

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace EmployeProblem
             int Is_Full_Time = 1;
             Random randomnum = new Random();
             int EmpCheck = randomnum.Next(0, 2);
+            int full_time_salary = 20*8;
             if (EmpCheck == Is_Full_Time)
             {
                 Console.WriteLine("Employee is Present");
+                Console.WriteLine("Daily salary of Employe {0}",full_time_salary);
 
             }
             else

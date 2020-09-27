8065a86ece7ea230a0e3d01bc0831af7467daa42
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-27 22:25:29 +0530

    for UC1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            Random randomnum = new Random();
+            int EmpCheck = randomnum.Next(0, 2);
+            if (EmpCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}

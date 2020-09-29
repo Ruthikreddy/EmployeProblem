4f59a0e345b098824e588696d1bf08719927a59a
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-29 22:02:38 +0530

    Commiting for UC8

--- Program.cs
+++ Program.cs
@@ -2,49 +2,93 @@
 
 namespace EmployeProblem
 {
-    class Program
+    class EmployeeWageComputation
     {
-        
-        static void Main(string[] args)
-        {
-            EmpWage("Capgemini",8,20,100);
-            EmpWage("Visualstudio", 5, 30, 120);
-        }
-        static void EmpWage(string Companyname,int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAYS,int MAX_WORKING_HOURS)
-        {
-            const int IS_ABSENT = 0;
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
-       
-            Random randomnumber = new Random();
-            int empHours = 0, totalWorkingHours = 0;
+        const int IS_FULL_TIME = 1;
+        const int ISS_PART_TIME = 2;
+        private string CompanyName;
+        private int maxWorkingHours;
+        private int maxNumOfWorkingDays;
+        private int ratePerHour;
+        private int totalWage = 0;
 
-            for (int Day = 1; Day <= NUM_OF_WORKING_DAYS; Day++)
+        public EmployeeWageComputation(string companyName, int maxWorkingHours, int maxNumOfWorkingDays, int ratePerHour)
+        {
+            this.CompanyName = companyName;
+            this.maxWorkingHours = maxWorkingHours;
+            this.maxNumOfWorkingDays = maxNumOfWorkingDays;
+            this.ratePerHour = ratePerHour;
+
+        }
+
+        public void calculateEmpWage()
+        {
+            int empHrs;
+
+            int totalEmpHours = 0;
+            int workingDays = 0;
+            Random rand = new Random();
+            while (totalEmpHours < maxWorkingHours && workingDays < maxNumOfWorkingDays)
+
             {
-                int EmpCheck = randomnumber.Next(0, 3);
-                switch (EmpCheck)
+
+                int empcheck = rand.Next(0, 3);
+
+
+
+                switch (empcheck)
                 {
-                    case IS_ABSENT:
-                        empHours = 0;
-                        break;
-                    case IS_PART_TIME:
-                        empHours = 4;
-                        break;
                     case IS_FULL_TIME:
-                        empHours = 8;
+                        empHrs = 8;
+                        break;
+                    case ISS_PART_TIME:
+                        empHrs = 4;
                         break;
                     default:
+                        empHrs = 0;
                         break;
+
+
                 }
-                if (totalWorkingHours + empHours > MAX_WORKING_HOURS)
-                    empHours = 0;
-                totalWorkingHours += empHours;
-                Console.WriteLine("Day " + (Day) + " Employee Hours " + empHours);
+                totalEmpHours = totalEmpHours + empHrs;
+                Console.WriteLine("the total working days is " + workingDays + "and working hours is " + totalEmpHours);
+
+                workingDays++;
             }
-            Console.WriteLine("Total Employee Hours " + totalWorkingHours);
-            int totalEmpWage = totalWorkingHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage for "+ Companyname +":"+ totalEmpWage);
+
+            totalWage = totalEmpHours * ratePerHour;
+            Console.WriteLine("The total wage of worker for" + CompanyName + "is:" + totalWage);
+
+
+        }
+        public string information()
+        {
+            return "Total wage for" + this.CompanyName + "is:" + this.totalWage;
+        }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            EmployeeWageComputation Capgemini = new EmployeeWageComputation("Capgemini", 30, 70, 20);
+            EmployeeWageComputation Amazon = new EmployeeWageComputation("Amazon", 20, 60, 40);
+            Capgemini.calculateEmpWage();
+            Amazon.calculateEmpWage();
+            Console.WriteLine(Capgemini.information());
+            Console.WriteLine(Amazon.information());
+
+
         }
 
+
     }
+
+
+
+
+
+
 }

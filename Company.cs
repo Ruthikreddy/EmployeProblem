99e7ce73570d93eca9fd094636fb2bf02f78d6a8
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-30 20:50:03 +0530

    Push for UC13

--- Company.cs
+++ Company.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,17 +10,31 @@ namespace Employee_Wage_Computation1
 
         internal string companyName;
         internal int wagePerHour;
-        internal int maxWorkingDays;
+        internal int maxWorkingDays = 0;
         internal int maxWorkingHours;
         internal int totalEmpWage;
+        public int workingDay;
+        public Dictionary<int, int> dailyWage = new Dictionary<int, int>();
+
+
         public Company(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
         {
             this.companyName = companyName;
             this.wagePerHour = wagePerHour;
             this.maxWorkingDays = maxWorkingDays;
             this.maxWorkingHours = maxWorkingHours;
+            this.workingDay = 1;
 
         }
+
+        public void saveDailyWage(int dailyWage)
+        {
+
+            this.dailyWage.Add(workingDay, dailyWage);
+            workingDay++;
+
+        }
+
 
         public void saveTotalWage(int totalWage)
         {

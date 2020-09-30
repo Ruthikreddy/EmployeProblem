16fdba9479a2bdacb3d8c1d7fed297d5f75609b7
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-30 20:43:37 +0530

    pushing for UC10

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Employee_Wage_Computation1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            EmpWageCalculator empWageCalculator = new EmpWageCalculator();
+
+            //Add the details of all the companies first
+            empWageCalculator.AddCompanyDetails("Reliance", 60, 20, 100);
+            empWageCalculator.AddCompanyDetails("Flipkart", 40, 20, 80);
+
+            // Calculate the employee wage of all the companies
+            empWageCalculator.CalculateTotalEmpWage();
+
+        }
+
+
+
+
+
+
+
+    }
+}
